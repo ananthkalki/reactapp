e74f928ebe397b7a5c334ae72c721fd15f89f5d8
Ananth Kalki <ananthkalki@gmail.com>
2021-04-27 17:42:21 +0530

    2nd update with props and components

--- src/app components/app.js
+++ src/app components/app.js
@@ -2,14 +2,20 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 import Note from './note';
+import notes from "../notes"
+
+function createNote(notes){
+    return(
+        <Note key={notes.key} title={notes.title} content={notes.content} />
+    );
+}
 
 function App (){
  return(
      <div className="app-container">
         <Header />
         <div className="main-section">
-            <Note />
-            
+            {notes.map(createNote)}   
         </div>
         <Footer />
      </div>
